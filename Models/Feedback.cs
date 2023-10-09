fccb361a1c1f00691ec70a5ccfef8423020ce8cf
anhTuann <tstnatuan@gmail.com>
2023-10-10 06:36:53 +0700

    Done Feedback V1.1

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,6 +1,21 @@
-﻿namespace back_end.Models
+﻿using back_end.Entities;
+
+namespace back_end.Models
 {
     public class Feedback
     {
+        public int FeedbackId { get; set; }
+
+        public string? FeedbackDetail { get; set; }
+
+        public int? MemberId { get; set; }
+
+        public int? ServiceId { get; set; }
+
+        public DateTime? FeedbackDate { get; set; }
+
+       
+
+        
     }
 }
