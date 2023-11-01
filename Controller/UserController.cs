09af54de7146799ba1ecfa405665cb3aa3662731
anhTuann <tstnatuan@gmail.com>
2023-11-01 19:25:11 +0700

    Update UserController V1.3

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using back_end.Entities;
+using back_end.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -27,5 +28,32 @@ namespace back_end.Controller
             }).FirstOrDefaultAsync(user => user.UserId == userID);
             return Ok(userInfo);
         }
+
+        [HttpPut("UpdateUser/{userID}")]
+        public async Task<IActionResult> UpdateUser([FromRoute] int userID, [FromBody] UserUpdateModel userUpdate)
+        {
+            var user = await _context.TblUsers.FirstOrDefaultAsync(u => u.UserId == userID);
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            // Cập nhật thông tin người dùng
+            user.UserName = userUpdate.UserName;
+            user.Email = userUpdate.Email;
+            user.FullName = userUpdate.FullName;
+            user.PhoneNumber = userUpdate.PhoneNumber;
+
+            // Kiểm tra xem userUpdate.Image có giá trị mới hay không và cập nhật ảnh nếu có
+            if (!string.IsNullOrEmpty(userUpdate.Image))
+            {
+                user.Image = userUpdate.Image;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
     }
 }
